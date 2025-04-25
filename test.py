fb0132d0a8026c2f3b9e989dcb7c93730c7afe60
KANZEZ <11912705@mail.sustech.edu.cn>
2025-04-24 22:50:29 -0400

    add 2d modulation, need clean

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import torch
 from model import LPV_NN, single_LPV
 import pandas as pd
 
+from modulation import get_M
+
 
 # create the data points
 limits = (0, 5, 0, 5)
@@ -30,6 +32,7 @@ max_v = min(max_vx, max_vy)
 
 # get the model output (velocity)
 vel = np.zeros((pos.shape[0], 2))
+modi_vel = np.zeros((pos.shape[0], 2))
 lyap_values = np.zeros((pos.shape[0],))
 for i in range(pos.shape[0]):
     input = torch.tensor(pos[i, :], dtype=torch.float32, requires_grad=True).unsqueeze(0)
@@ -40,13 +43,17 @@ for i in range(pos.shape[0]):
 
     lyap_values[i] = V.item()
 
-
+    ##### after modulation:
+    modyfied_vel = get_M(pos[i, 0], pos[i, 1]) @ vel[i, :].T
+    modi_vel[i, :] = modyfied_vel.T
 
 # plot the map
 X = pos[:, 0]
 Y = pos[:, 1]
 U = vel[:, 0]
 V = vel[:, 1]
+MU = modi_vel[:, 0]
+MV = modi_vel[:, 1]
 speed = np.linalg.norm(vel, axis=1) / 1000
 Lyapunov_grid = lyap_values.reshape((len(y), len(x)))
 
@@ -57,7 +64,10 @@ contour = ax.contourf(x, y, Lyapunov_grid, levels=50, cmap='cividis')
 plt.colorbar(contour, ax=ax, label="Lyapunov Value")
 
 # policy
-ax.quiver(X, Y, U, V, speed, cmap='Reds', scale=20, scale_units='xy')
+#ax.quiver(X, Y, U, V, speed, cmap='Reds', scale=20, scale_units='xy') ### ORIGINAL POLICY
+
+ax.quiver(X, Y, MU, MV, speed, cmap='Blues', scale=20, scale_units='xy') ### MODIFIED POLICY
+
 ax.set_xlim(limits[0], limits[1])
 ax.set_ylim(limits[2], limits[3])
 ax.set_aspect('equal')
